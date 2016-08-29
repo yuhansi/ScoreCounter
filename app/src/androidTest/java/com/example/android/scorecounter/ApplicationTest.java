4afcfeb6c784821814f293d4a9b16cd50ad8014d
Yuhan Si <yuhansi@usc.edu>
2016-08-28 21:04:19 -0700

    updated files

--- app/src/androidTest/java/com/example/android/scorecounter/ApplicationTest.java
+++ app/src/androidTest/java/com/example/android/scorecounter/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.android.courtcounter;
+package com.example.android.scorecounter;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/example/android/scorecounter/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/scorecounter/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.android.courtcounter;
+package com.example.android.scorecounter;
 
 import org.junit.Test;
 
